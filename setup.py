1b560fb81af07c5794bbbec2578a922b660c449c
gjkhai <38274720+gjkhai@users.noreply.github.com>
2026-02-04 19:45:14 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="customplottinglibrary",
+    version="1.0.0",
+    packages=["customplottinglibrary"],
+    url="https://github.com/ganjk/custom-plotting-library/",
+    author="@ganjk",
+    description="Custom matplotlib charts for trading & financial related data",
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "setuptools",
+        "pandas",
+        "numpy",
+        "matplotlib"
+    ],
+)
